08805ef2c213ba72042e8dd288812ef75d93adba
CarlesRojas <carlesrojas@outlook.com>
2023-11-08 13:36:27 +0100

    update config

--- package/rollup.config.js
+++ package/rollup.config.js
@@ -9,47 +9,48 @@ import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
 
 const config = [
-  {
-    input: './src/index.ts',
+    {
+        input: './src/index.ts',
 
-    output: [
-      {
-        sourcemap: true,
-        file: './dist/index.js',
-        format: 'esm',
-        globals: { react: 'React' }
-      }
-    ],
+        output: [
+            {
+                sourcemap: true,
+                file: './dist/index.js',
+                format: 'esm',
+                globals: { react: 'React' },
+                inlineDynamicImports: true
+            }
+        ],
 
-    plugins: [
-      typescript({
-        useTsconfigDeclarationDir: true
-      }),
-      peerDepsExternal(),
-      postcss({
-        extract: false,
-        modules: true,
-        extensions: ['.scss'],
-        use: ['sass']
-      }),
-      babel({ exclude: 'node_modules/**' }),
-      resolve(),
-      commonjs(),
-      copy({
-        targets: [
-          { src: 'src/tokens/tokens.scss', dest: 'dist' },
-          { src: 'README.md', dest: 'dist' }
-        ]
-      })
-    ],
+        plugins: [
+            typescript({
+                useTsconfigDeclarationDir: true
+            }),
+            peerDepsExternal(),
+            postcss({
+                extract: false,
+                modules: true,
+                extensions: ['.scss'],
+                use: ['sass']
+            }),
+            babel({ exclude: 'node_modules/**' }),
+            resolve(),
+            commonjs(),
+            copy({
+                targets: [
+                    { src: 'src/tokens/tokens.scss', dest: 'dist' },
+                    { src: 'README.md', dest: 'dist' }
+                ]
+            })
+        ],
 
-    external: ['react', 'react-dom']
-  },
-  {
-    input: './dist/dts/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'es' }],
-    plugins: [dts(), del({ hook: 'buildEnd', targets: './dist/dts' })]
-  }
+        external: ['react', 'react-dom']
+    },
+    {
+        input: './dist/dts/index.d.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts(), del({ hook: 'buildEnd', targets: './dist/dts' })]
+    }
 ];
 
 export default config;
